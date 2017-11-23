c05d125385e58578e6c304a76a6527702b6bf747
ShivaKalantar <sh.kalantar1991@gmail.com>
2017-11-23 17:06:10 +0330

    A

---
+++ js/equal-heights.js
@@ -0,0 +1,224 @@
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".A").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".A").height(maxHeight);
+        }, 300);
+    }else{
+        $(".A").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".B").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".B").height(maxHeight);
+        }, 300);
+    }else{
+        $(".B").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".C").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".C").height(maxHeight);
+        }, 300);
+    }else{
+        $(".C").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".D").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".D").height(maxHeight);
+        }, 300);
+    }else{
+        $(".D").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".E").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".E").height(maxHeight);
+        }, 300);
+    }else{
+        $(".E").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".F").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".F").height(maxHeight);
+        }, 300);
+    }else{
+        $(".F").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".G").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".G").height(maxHeight);
+        }, 300);
+    }else{
+        $(".G").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".H").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".H").height(maxHeight);
+        }, 300);
+    }else{
+        $(".H").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".I").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".I").height(maxHeight);
+        }, 300);
+    }else{
+        $(".I").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".J").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".J").height(maxHeight);
+        }, 300);
+    }else{
+        $(".J").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".K").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".K").height(maxHeight);
+        }, 300);
+    }else{
+        $(".K").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".L").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".L").height(maxHeight);
+        }, 300);
+    }else{
+        $(".L").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".M").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".M").height(maxHeight);
+        }, 300);
+    }else{
+        $(".M").css({'height':'auto'});
+    }
+});
+$(window).on("load resize", function(){
+    /*-- Set Height ---*/
+    var heights = $(".N").map(function ()
+        {
+            return $(this).height();
+        }).get(),
+
+        maxHeight = Math.max.apply(null, heights)+'px';
+    if($('#visMd').is(":visible")) {
+        setTimeout(function () {
+            $(".N").height(maxHeight);
+        }, 300);
+    }else{
+        $(".N").css({'height':'auto'});
+    }
+});
